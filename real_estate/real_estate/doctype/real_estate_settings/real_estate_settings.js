cb9cf7e85597de98b96cd22a2d7a84579566e24d
Ashish <ashish@greycube.in>
2018-07-25 03:34:21 +0000

    jignesh print 1

---
+++ real_estate/real_estate/doctype/real_estate_settings/real_estate_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, GreyCube Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Real Estate Settings', {
+	refresh: function(frm) {
+
+	}
+});

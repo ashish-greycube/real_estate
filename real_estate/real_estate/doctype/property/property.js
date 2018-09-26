774150dc32f3517ef1e5369e432c1b1b8ee2c33c
ashish-greycube <mr.ashish.shah@gmail.com>
2018-09-26 15:35:37 +0530

    property url give docname

--- real_estate/real_estate/doctype/property/property.js
+++ real_estate/real_estate/doctype/property/property.js
@@ -72,7 +72,7 @@ frappe.ui.form.on('Property', {
 	},
 
 	validate: function (frm) {
-		frm.set_value('signup_form_url',frappe.urllib.get_base_url()+'/property-lead?for_property='+encodeURIComponent(cur_frm.doc.property_name))
+		frm.set_value('signup_form_url',frappe.urllib.get_base_url()+'/property-lead?for_property='+cur_frm.doc.name)
 
 	},
 	refresh: function (frm, cdt, cdn) {

c8b8fe635070cf587ff2981ca6fcb378235c6bc8
ashish-greycube <mr.ashish.shah@gmail.com>
2018-09-27 17:19:49 +0530

    clear menu

--- real_estate/real_estate/doctype/property/property_list.js
+++ real_estate/real_estate/doctype/property/property_list.js
@@ -1,5 +1,14 @@
 frappe.listview_settings['Property'] = {
+	onload: function(listview) {
+
+	},
 	refresh: function(listview) {
 		listview.page.clear_menu()
+		listview.page.add_menu_item(__("Clear Filter"), function() {
+			frappe.route_options = {'property_name':["like",'%%']}
+            frappe.set_route("List", "Property");
+			cur_list.refresh();
+		});
+
 	}
 };

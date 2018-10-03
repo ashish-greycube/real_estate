318ff27b60714071533493a5b5f75e3f151c0d93
ashish-greycube <mr.ashish.shah@gmail.com>
2018-10-03 06:43:56 +0530

    phase 1 - property list error

--- real_estate/real_estate/doctype/property_transaction/property_transaction_list.js
+++ real_estate/real_estate/doctype/property_transaction/property_transaction_list.js
@@ -16,8 +16,9 @@ frappe.listview_settings['Property Transaction'] = {
 	},
 	onload: function(listview) {
 		console.log('inside')
-		if (frappe.get_prev_route()[0]=="home-page") {
-			console.log('inside')
+		if (frappe.get_prev_route()[0]=="home-page" && frappe.get_route_str()=="List/Property Transaction/Calendar/Default"
+		) {
+			console.log('1inside')
 		this.wrapper.find("a.remove-filter").on("click", function() {
 			me.remove();
 		});
